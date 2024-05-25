b0af7ccbeea4bf3666282ae86b6b0f17b72fbfa1
MuV1N1 <Marvin.kleiner1910@gmail.com>
2024-05-25 14:45:56 +0200

    yay

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <header>
+      <div class="nav">
+        <ul>
+          <li class="nal"><a href=""><i class="fa-solid fa-music"></i></a></li>
+          <li class="nar"><a href="start-section">test</a></li>
+          <li class="nar"><a href="s2">test</a></li>
+          <li class="nar"><a href="s3">test</a></li>
+          <li class="nar"><a href="s4">test</a></li>
 
-      </header>
-      <main>
-        <section class="section-image"><img src="https://www.djrob.de/images/djrob-visitenkarte.png"></img></section>
-      </main>
-      <footer>
-
-      </footer>
+        </ul>
+      </div>
+      <div>
+        <section id="start-section">
+          <div id="card-image"><img class="start-img" src="/MichaelKarte.jpg"></img></div>
+          <div class="flex">
+            <h3>TEST</h3>
+          </div>
+        </section>
+        <section id="s2" class="flex"><p>Test</p></section>
+        <section  id="s3" class="flex"><p>Test</p></section>
+        <section id="s4" class="flex"><p>Test</p></section>
+      </div>
     </>
   )
 }
